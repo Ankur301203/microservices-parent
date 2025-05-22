4a73d18c47cfa9e7b43fbebfa9e23b21759c1fb7
Ankur Kumar <ankur.301203@gmail.com>
2025-05-22 18:20:50 +0530

    Update some configurations in multiple application.properties files of various services

--- api-gateway/src/main/java/org/ankur/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/ankur/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,14 @@ package org.ankur.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.function.Predicate;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+
     }
 
 }
